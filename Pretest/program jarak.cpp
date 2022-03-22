6b29ec22d874eb2f2cc616589ec65d6f3057e76d
rajuputra <raju2100018316@webmail.uad.ac.id>
2022-03-22 19:20:29 +0700

    Studi kasus

---
+++ Pretest/program jarak.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#incluee <conio.h>
+
+using namespace std ;
+
+int main (){
+	float x1,y1,x2,y2;
+	cout << " Program Menghitung Jarak antara 2 titik \n" ;
+	cout << " Masukkan koordinat titik pertama \n ;
+	cout << " x1 = " ; cin >> x1;
+	cout << " y1 = " ; cin >> y1;
+	cout << " titik pertama A(" <<x1<<","<<y1")" << endl << endl;
+	cout << " Masukkan koordinat titik kedua \n ;
+	cout << " x2 = " ; cin >> x2;
+	cout << " y2 = " ; cin >> y2;
+	cout << " titik pertama B(" <<x2<<","<<y2")" << endl << endl;
+	cout << " "
+	
+	return 0;
+}
